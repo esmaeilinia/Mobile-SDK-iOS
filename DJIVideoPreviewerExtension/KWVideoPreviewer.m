79aaa68a0c3db77cc33614cfe7e2ed161a247a21
Pandara <wen.pandara@gmail.com>
2018-09-14 15:20:53 +0800

    refactor: Use Subclassing to implement VideoPreviewer delegate
    The selector "setDelegate" can not be found when the host project use DJIWidget and DJI-UXSDK-iOS at the same time

--- DJIVideoPreviewerExtension/KWVideoPreviewer.m
+++ DJIVideoPreviewerExtension/KWVideoPreviewer.m
@@ -1,56 +1,22 @@
 //
-//  DJIVideoPreviewer+RetrievingDeocdedBuffer.m
-//  Mesh
+//  KWVideoPreviewer.m
+//  DJISdkDemo
 //
-//  Created by Pandara on 2018/9/3.
-//  Copyright © 2018 Kiwi Information Technology Co., Ltd. All rights reserved.
+//  Created by Pandara on 2018/9/14.
+//  Copyright © 2018 DJI. All rights reserved.
 //
 
-#import "DJIVideoPreviewer+RetrieveDeocdedBuffer.h"
-#import <objc/runtime.h>
-#import <CoreVideo/CoreVideo.h>
-#import "DJIVideoPreviewerDelegate.h"
-#import "DJIVideoPreviewer+Delegate.h"
+#import "KWVideoPreviewer.h"
 
-@implementation DJIVideoPreviewer (RetrieveDeocdedBuffer)
+@implementation KWVideoPreviewer
 
-+ (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        
-        SEL originalSelector = @selector(videoProcessFrame:);
-        SEL swizzlingSelector = @selector(swizzling_videoProcessFrame:);
-        
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        IMP originalIMP = method_getImplementation(originalMethod);
-        const char *originalTypes = method_getTypeEncoding(originalMethod);
-        
-        NSAssert(originalIMP, @"\
-                 没有找到 DJIVideoPreviewer 中的 videoProcessFrame 方法，\
-                 可能 DJI 对实现细节进行了修改，\
-                 需要重新找到类似职责的方法来进行 hook");
-        
-        Method swizzlingMethod = class_getInstanceMethod(class, swizzlingSelector);
-        IMP swizzlingIMP = method_getImplementation(swizzlingMethod);
-        const char *swizzlingTypes = method_getTypeEncoding(swizzlingMethod);
-        
-        class_replaceMethod(class, swizzlingSelector, originalIMP, originalTypes);
-        class_replaceMethod(class, originalSelector, swizzlingIMP, swizzlingTypes);
-    });
-}
-
-/// 在运行时与 DJIVideoPreviewer 中的 videoProcessFrame 方法进行交换
-- (void)swizzling_videoProcessFrame:(VideoFrameYUV *)frame {
-    // TODO: 在 DJIVideoPreviewer 的实现中，不是所有 buffer 都需要显示，
-    // 有提前退出的逻辑，但此处通过 delegate 传送出去的 buffer 没有考虑这层逻辑
-    
-    if (self.delegate) {
+- (void)videoProcessFrame:(VideoFrameYUV *)frame {
+    if ([self.delegate respondsToSelector:@selector(kwVideoPreviewer:willProcessImageBuffer:)]) {
         CVImageBufferRef imageBuffer = (CVImageBufferRef)CFRetain((CFTypeRef)[self convertToImageBufferFromYUVFrame:frame]);
-        [self.delegate djiVideoPreviewer:self willProcessImageBuffer:imageBuffer];
+        [self.delegate kwVideoPreviewer:self willProcessImageBuffer:imageBuffer];
         CFRelease(imageBuffer);
     }
-    [self swizzling_videoProcessFrame:frame];
+    [super videoProcessFrame:frame];
 }
 
 - (CVImageBufferRef)convertToImageBufferFromYUVFrame:(VideoFrameYUV *)yuvFrame {
@@ -165,18 +131,3 @@
 }
 
 @end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
